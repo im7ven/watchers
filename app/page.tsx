824b5a8577bb03ea756d331bfa3f1f00b9c63b0b
Justin <alexanderrr416@gmail.com>
2024-08-27 17:35:27 -0400

    chore: wrapping application with session provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+"use client";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
+  const { data } = useSession();
   return (
     <main>
+      {data && data.user?.name}
       <Link href="api/auth/signin">Login</Link>
+      <Link href="api/auth/signout">Logout</Link>
     </main>
   );
 }

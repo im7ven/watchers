712fd85a14ccebb0c6dda4d37f04a5d831b72aa6
Justin <alexanderrr416@gmail.com>
2024-09-09 20:25:16 -0400

    chore: generate new api key

--- app/services/api-client.ts
+++ app/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "4e744e9e8e134827cdae45391d5038e2",
+    api_key: process.env.NEXT_PUBLIC_TMDB_API_KEY,
   },
 });

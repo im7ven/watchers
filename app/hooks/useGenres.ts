6a824bb92e2000aa6440afcade29083d5f294e17
Justin <alexanderrr416@gmail.com>
2024-09-19 23:30:36 -0400

    chore: create hook for fetching movie genres

---
+++ app/hooks/useGenres.ts
@@ -0,0 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
+
+type Genre = {
+  id: number;
+  name: string;
+};
+
+type GenreResponse = {
+  genres: Genre[];
+};
+
+const fetchGenres = async () => {
+  try {
+    const { data } = await apiClient.get<GenreResponse>("/genre/movie/list");
+    return data;
+  } catch (error) {
+    throw new Error(
+      "Sorry an unexpected error occurred while fetching the genres."
+    );
+  }
+};
+
+const useGenres = () => {
+  return useQuery({
+    queryKey: ["genres"],
+    queryFn: fetchGenres,
+  });
+};
+
+export default useGenres;

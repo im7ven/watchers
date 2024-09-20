ab00cccc7fa44b4e52cd4b3be2f0e501aebf2099
Justin <alexanderrr416@gmail.com>
2024-09-19 22:28:06 -0400

    chore: create a hook for fetching movie details

--- app/hooks/useMovieDetails.tsx
+++ app/hooks/useMovieDetails.tsx
@@ -6,18 +6,77 @@ type Genre = {
   name: string;
 };
 
+type Cast = {
+  id: number;
+  name: string;
+  character: string;
+  profile_path?: string;
+};
+
+type Crew = {
+  id: number;
+  name: string;
+  profile_path?: string;
+};
+
+type Credit = {
+  cast: Cast[];
+  crew: Crew[];
+};
+
+type Video = {
+  key: string;
+  site: string;
+  type: string;
+};
+
+type Videos = {
+  results: Video[];
+};
+
+type Rating = {
+  rating: string;
+};
+
+type ContentRating = {
+  results: Rating[];
+};
+
+type Media = {
+  id: number;
+  title: string;
+  poster_path?: string;
+};
+
+type SimilarMedia = {
+  results: Media[];
+};
+
 type Movie = {
   id: number;
   title: string;
-  poster_path: string;
+  poster_path?: string;
   genres: Genre[];
   budget: number;
   overview: string;
+  release_date: string;
+  runtime: number;
+  revenue: number;
+  tagline: string;
+  vote_average: number;
+  original_language: string;
+  credits: Credit;
+  videos: Videos;
+  content_ratings: ContentRating;
+  status: string;
+  similar: SimilarMedia;
 };
 
 const fetchMovieDetails = async (movieId: string) => {
   try {
-    const { data } = await apiClient.get<Movie>(`/movie/${movieId}`);
+    const { data } = await apiClient.get<Movie>(
+      `/movie/${movieId}?append_to_response=credits,videos,similar`
+    );
     return data;
   } catch (error) {
     throw new Error(

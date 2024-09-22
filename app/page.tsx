8353f31efab71e456db77bbf79ec7d09fc86ed35
Justin <alexanderrr416@gmail.com>
2024-09-22 17:56:55 -0400

    fix: correct movie grid responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,6 @@ type Movie = {
 const posterUrl = `https://image.tmdb.org/t/p/w500`;
 
 export default function Home() {
-  const { data: sessionData } = useSession();
   const [movies, setMovies] = useState<Movie[]>([]);
   const [loading, setLoading] = useState(true);
 
@@ -37,22 +36,12 @@ export default function Home() {
 
   return (
     <main>
-      {sessionData && <p>Welcome, {sessionData.user?.name}</p>}
-      <Button>Testing Radix</Button>
-      <Link href="api/auth/signin">Login</Link>
-      <Link href="api/auth/signout">Logout</Link>
-
-      <Grid
-        columns={{ initial: "3", sm: "5", md: "6" }}
-        gap="1"
-        maxWidth={{ initial: "450px", sm: "800px", md: "1000px" }}
-        mx="auto"
-      >
+      <Grid columns={{ initial: "3", xs: "5", sm: "6" }} gap="1" mx="auto">
         {movies.map((movie) => (
           <Box key={movie.id}>
             <Link href={`/movie/${movie.id}`}>
               <Image
-                width={150}
+                width={165}
                 height={300}
                 src={posterUrl + movie.poster_path}
                 alt="poster"

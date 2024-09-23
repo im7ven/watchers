e55bb2ef8ddfcb0694fd6181486d2be038130ad2
Justin <alexanderrr416@gmail.com>
2024-09-22 22:16:03 -0400

    chore: create a generic media data fetching hook

---
+++ app/hooks/useData.tsx
@@ -0,0 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
+
+type MediaResponse<T> = {
+  results: T[];
+};
+
+const useData = <T,>(endpoint: string, key: string[]) => {
+  return useQuery({
+    queryKey: key,
+    queryFn: async () => {
+      try {
+        const { data } = await apiClient.get<MediaResponse<T>>(endpoint);
+        console.log("HEREEE", data);
+        return data;
+      } catch (error) {
+        throw new Error(
+          "Sorry, an unexpected error has occurred please try again later."
+        );
+      }
+    },
+  });
+};
+
+export default useData;

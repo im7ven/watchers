9a5c95520694c6e26bc452741fe5289b20d6a74d
Justin <alexanderrr416@gmail.com>
2024-09-25 19:54:32 -0400

    feat: fetch media base on selected tab

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { Button, Grid, Box } from "@radix-ui/themes";
-import { useSession } from "next-auth/react";
+import { Box, Flex, Grid, Tabs, Text } from "@radix-ui/themes";
 import Image from "next/image";
 import Link from "next/link";
-import apiClient from "./services/api-client";
+import { useEffect, useState } from "react";
+import usePopularMovie from "./hooks/usePopularMovie";
+import useData from "./hooks/useData";
 
 type Movie = {
   id: number;
@@ -16,30 +16,93 @@ type Movie = {
 const posterUrl = `https://image.tmdb.org/t/p/w500`;
 
 export default function Home() {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [loading, setLoading] = useState(true);
+  const [selectedTab, setSelectedTab] = useState("/movie/popular");
+  const [isMovieSelected, setIsMovieSelected] = useState("movie");
+  const { data } = useData<Movie>(selectedTab, [selectedTab]);
+  const [defaultTabValue, setDefaultTabValue] = useState("/movie/popular");
 
-  const getPopularMovies = async () => {
-    try {
-      const { data: popularMovies } = await apiClient.get("/movie/popular");
-      setMovies(popularMovies.results); // Assuming `results` is the array of movies from the API response
-    } catch (error) {
-      console.error("Error fetching movies:", error);
-    } finally {
-      setLoading(false);
-    }
+  const onSelectMovie = () => {
+    setIsMovieSelected("movie");
+    setSelectedTab("movie/popular");
   };
 
-  useEffect(() => {
-    getPopularMovies();
-  }, []);
+  const onSelectTv = () => {
+    setIsMovieSelected("tv");
+    setSelectedTab("tv/popular");
+  };
+
+  // useEffect(() => {
+  //   const defaultTabValue = isMovieSelected ? "/movie/popular" : "/tv/popular";
+  //   setDefaultTabValue(defaultTabValue);
+  // }, [isMovieSelected]);
+
+  // console.log(selectedTab);
 
   return (
     <main>
+      <Flex justify="between" align="center">
+        <Flex className="flex-1 text-[rgba(255,255,255,.5)]" gap="2">
+          <Text
+            onClick={onSelectMovie}
+            className={`cursor-pointer ${
+              isMovieSelected === "movie" ? "font-semibold text-white" : ""
+            }`}
+          >
+            Movie
+          </Text>
+          |
+          <Text
+            onClick={onSelectTv}
+            className={` cursor-pointer ${
+              isMovieSelected === "tv" ? "font-semibold text-white" : ""
+            }`}
+          >
+            TV
+          </Text>
+        </Flex>
+        {isMovieSelected === "movie" && (
+          <Tabs.Root
+            defaultValue={"/movie/popular"}
+            onValueChange={(value) => setSelectedTab(value)}
+          >
+            <Tabs.List
+              size={{ initial: "1", xs: "2" }}
+              className="items-center"
+              justify="center"
+            >
+              <Tabs.Trigger value="/movie/popular">Popular</Tabs.Trigger>
+              <Tabs.Trigger value="/movie/now_playing">
+                Now Playing
+              </Tabs.Trigger>
+              <Tabs.Trigger value="/movie/upcoming">Upcoming</Tabs.Trigger>
+            </Tabs.List>
+          </Tabs.Root>
+        )}
+        {isMovieSelected === "tv" && (
+          <Tabs.Root
+            defaultValue={"/tv/popular"}
+            onValueChange={(value) => setSelectedTab(value)}
+          >
+            <Tabs.List
+              size={{ initial: "1", xs: "2" }}
+              className="items-center"
+              justify="center"
+            >
+              <Tabs.Trigger value="/tv/popular">Popular</Tabs.Trigger>
+              <Tabs.Trigger value="/trending/tv/week">Trending</Tabs.Trigger>
+              <Tabs.Trigger value="/tv/top_rated">Top Rated</Tabs.Trigger>
+            </Tabs.List>
+          </Tabs.Root>
+        )}
+      </Flex>
       <Grid columns={{ initial: "3", xs: "5", sm: "6" }} gap="1" mx="auto">
-        {movies.map((movie) => (
+        {data?.results.map((movie) => (
           <Box key={movie.id}>
-            <Link href={`/movie/${movie.id}`}>
+            <Link
+              href={`/${isMovieSelected === "movie" ? "movie" : "tv"}/${
+                movie.id
+              }`}
+            >
               <Image
                 width={165}
                 height={300}

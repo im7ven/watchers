fe9c5f4b7c2d0e6b3e2a44dbdc5df281c08261b4
Justin <alexanderrr416@gmail.com>
2024-09-29 17:54:04 -0400

    style: apply layout styles

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Flex, TextField } from "@radix-ui/themes";
+import { Button, Flex, TextField } from "@radix-ui/themes";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import UserDropDown from "./UserDropDown";
@@ -9,10 +9,10 @@ const Navbar = () => {
   const { data: sessionData, status } = useSession();
 
   return (
-    <header className="border-b border-[rgba(255,255,255,.2)] py-3">
+    <header className="border-b border-[rgba(255,255,255,.2)] py-3 px-3">
       <nav>
         <Flex justify="between" align="center" gap="5">
-          <h1>LOGO</h1>
+          <Link href="/">LOGO</Link>
           <TextField.Root className="w-full" placeholder="Search the docs…">
             <TextField.Slot>
               <BsSearch />
@@ -21,7 +21,9 @@ const Navbar = () => {
           {status === "authenticated" ? (
             <UserDropDown session={sessionData} />
           ) : (
-            <Link href="api/auth/signin">Login</Link>
+            <Link href="api/auth/signin">
+              <Button>Login</Button>
+            </Link>
           )}
         </Flex>
       </nav>

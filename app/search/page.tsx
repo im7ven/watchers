047b7ec3ec962a4171a6d9d7494b8b0c7b9bb9ad
Justin <alexanderrr416@gmail.com>
2024-10-02 22:39:03 -0400

    style: refactor image layout

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,37 +1,49 @@
 "use client";
 
-import { Flex, Box, Text, Button } from "@radix-ui/themes";
+import { Flex, Box, Text, Button, Container, Badge } from "@radix-ui/themes";
 import React from "react";
 import useSearchData from "../hooks/useSearchData";
 import { useSearchValue } from "../contexts/SearchContext";
 import Image from "next/image";
 import Link from "next/link";
+import placeholder from "@/public/movie_placeholder.png";
 
 const SearchResultsPage = () => {
   const { searchVal } = useSearchValue();
-  const { data, fetchNextPage } = useSearchData(searchVal);
+  const { data } = useSearchData(searchVal);
+
+  const filteredData = data?.results.filter(
+    (media) => media.media_type !== "person"
+  );
   return (
     <>
-      <Flex direction="column">
-        {data?.pages.map((page, index) => (
-          <React.Fragment key={index}>
-            {page.results.map((media) => (
-              <Link key={media.id} href={`/${media.media_type}/${media.id}}`}>
-                <Flex>
-                  <Image
-                    src={`https://image.tmdb.org/t/p/w500${media.poster_path}`}
-                    width="150"
-                    height="300"
-                    alt={media.title}
-                  />
-                  <Text>{media.title}</Text>
-                </Flex>
-              </Link>
-            ))}
-          </React.Fragment>
+      <Flex p="3" direction="column" gap="2">
+        {filteredData?.map((media) => (
+          <Link key={media.id} href={`/${media.media_type}/${media.id}}`}>
+            <Flex gap="3">
+              <Container className="max-w-[100px] md:max-w-[120px] relative h-[150px] md:h-[180px]">
+                <Image
+                  src={
+                    media.poster_path
+                      ? "https://image.tmdb.org/t/p/w500" + media.poster_path
+                      : placeholder
+                  }
+                  fill
+                  alt={media.title || "Movie cover"}
+                />
+              </Container>
+              <Flex className="flex-1" direction="column" align="start">
+                <Text>
+                  {media.media_type === "tv" ? media.name : media.title}
+                </Text>
+                <Badge>
+                  {media.media_type === "tv" ? "TV Series" : "Movie"}
+                </Badge>
+              </Flex>
+            </Flex>
+          </Link>
         ))}
       </Flex>
-      <Button onClick={() => fetchNextPage()}>Load More</Button>
     </>
   );
 };

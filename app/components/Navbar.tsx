4f7cf3f03e6966ec352451c8c6c72def56e3d8f0
Justin <alexanderrr416@gmail.com>
2024-10-04 22:40:51 -0400

    fix: improve form validation ux

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { Button, Flex, TextField } from "@radix-ui/themes";
+import { Button, Flex, TextField, Text } from "@radix-ui/themes";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { BsSearch } from "react-icons/bs";
 import UserDropDown from "./UserDropDown";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
 import { useSearchValue } from "../contexts/SearchContext";
 
@@ -14,21 +14,32 @@ const Navbar = () => {
   const currentPath = usePathname();
   const { searchVal, setSearchVal } = useSearchValue();
   const searchRef = useRef<HTMLInputElement>(null);
-
-  console.log(router);
-  console.log("current", currentPath);
+  const [isInvalid, setIsInvalid] = useState(false);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (searchRef.current) {
+      if (searchRef.current.value.trim() === "") {
+        setIsInvalid(true);
+        return;
+      }
       setSearchVal(searchRef.current.value);
+      setIsInvalid(false);
     }
     if (currentPath !== "search") {
       router.push("/search");
     }
   };
 
-  console.log(searchVal);
+  const handleResetValidation = () => {
+    setIsInvalid(false);
+  };
+
+  const handleInputChange = () => {
+    if (isInvalid && searchRef.current?.value.trim() !== "") {
+      setIsInvalid(false);
+    }
+  };
 
   return (
     <header className="border-b border-[rgba(255,255,255,.2)] py-3 px-3">
@@ -37,14 +48,17 @@ const Navbar = () => {
           <Link href="/">LOGO</Link>
           <form onSubmit={handleSubmit}>
             <TextField.Root
+              onBlur={handleResetValidation}
+              onChange={handleInputChange}
               ref={searchRef}
-              className="w-full"
+              className={"w-full"}
               placeholder="Search the docs…"
             >
               <TextField.Slot>
                 <BsSearch />
               </TextField.Slot>
             </TextField.Root>
+            {isInvalid && <Text color="red">Please provide a value</Text>}
           </form>
           {status === "authenticated" ? (
             <UserDropDown session={sessionData} />

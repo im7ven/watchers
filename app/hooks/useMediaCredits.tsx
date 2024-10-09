d51f9f6bfff63542343c868cde6e33f28f1efed2
Justin <alexanderrr416@gmail.com>
2024-10-09 17:59:54 -0400

    fix: index with id

--- app/hooks/useMediaCredits.tsx
+++ app/hooks/useMediaCredits.tsx
@@ -8,16 +8,16 @@ const useMediaCredits = (mediaId: string, mediaType: string) => {
   const { data } = useMediaDetails(mediaId, mediaType);
 
   const crewSlides =
-    data?.credits.crew.map((crew, index) => ({
-      id: index,
+    data?.credits.crew.map((crew) => ({
+      id: crew.id,
       name: crew?.name,
       src: crew.profile_path ? posterPath + crew.profile_path : placeholderImg,
       alt: crew.name,
     })) || [];
 
   const castSlides =
-    data?.credits.cast.map((cast, index) => ({
-      id: index,
+    data?.credits.cast.map((cast) => ({
+      id: cast.id,
       name: cast?.name,
       character: cast.character,
       src: cast.profile_path ? posterPath + cast.profile_path : placeholderImg,
@@ -25,8 +25,8 @@ const useMediaCredits = (mediaId: string, mediaType: string) => {
     })) || [];
 
   const similarMediaSlides =
-    data?.similar.results.map((media, index) => ({
-      id: index,
+    data?.similar.results.map((media) => ({
+      id: media.id,
       name: media.title,
       src: media.poster_path
         ? posterPath + media.poster_path

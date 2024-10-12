d986ecf4082120d8bf953ce48c9f51ac3739dbfb
Justin <alexanderrr416@gmail.com>
2024-10-11 22:57:06 -0400

    fix: apply no referrer policy when fetching google user image

--- app/components/UserDropDown.tsx
+++ app/components/UserDropDown.tsx
@@ -1,8 +1,6 @@
-import { Avatar, Button, DropdownMenu } from "@radix-ui/themes";
+import { Avatar, DropdownMenu } from "@radix-ui/themes";
 import { Session } from "next-auth";
-import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 
 type Props = {
   session?: Session;
@@ -12,7 +10,12 @@ const UserDropDown = ({ session }: Props) => {
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger>
-        <Avatar radius="full" src={session?.user?.image!} fallback />
+        <Avatar
+          radius="full"
+          src={session?.user?.image!}
+          fallback
+          referrerPolicy="no-referrer"
+        />
       </DropdownMenu.Trigger>
       <DropdownMenu.Content>
         <DropdownMenu.Item>{session?.user?.name}</DropdownMenu.Item>

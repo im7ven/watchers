d13a5839c986f8bc100c67d137e67d924aea8598
Justin <alexanderrr416@gmail.com>
2024-10-15 15:05:17 -0400

    chore: build custom hook for fetching person combined credits

---
+++ app/hooks/usePersonCredits.tsx
@@ -0,0 +1,30 @@
+import usePerson from "./usePerson";
+import placeholder from "@/public/movie_placeholder.png";
+
+const posterPath = "https://image.tmdb.org/t/p/w500";
+
+const usePersonCredits = (personId: string) => {
+  const { data: person } = usePerson(personId);
+
+  const mediaCastSlides =
+    person?.combined_credits.cast?.map((media) => ({
+      id: media.id,
+      name: media.title,
+      src: media.poster_path ? posterPath + media.poster_path : placeholder,
+      alt: media.title,
+      media_type: media.media_type,
+    })) || [];
+
+  const mediaCrewSlides =
+    person?.combined_credits.crew?.map((media) => ({
+      id: media.id,
+      name: media.title,
+      src: media.poster_path ? posterPath + media.poster_path : placeholder,
+      alt: media.title,
+      media_type: media.media_type,
+    })) || [];
+
+  return { mediaCastSlides, mediaCrewSlides };
+};
+
+export default usePersonCredits;

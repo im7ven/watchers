f5847ec8917ca4598603452fd2841ddfb4166aee
Justin <alexanderrr416@gmail.com>
2024-10-15 16:39:24 -0400

    chore: enlarge media trailers

--- app/components/VideoPlayer.tsx
+++ app/components/VideoPlayer.tsx
@@ -8,7 +8,7 @@ type Props = {
 const VideoPlayer = ({ videoId, site }: Props) => {
   return (
     <iframe
-      className="mx-auto my-8"
+      className="mx-auto my-8 aspect-video w-full max-w-[500px]"
       src={`https://${site}.com/embed/${videoId}`}
     >
       VideoPlayer

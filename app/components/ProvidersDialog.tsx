de9955d7a19bbfd46ffd7f121e1e47e989ca756b
Justin <alexanderrr416@gmail.com>
2024-10-16 20:11:45 -0400

    feat: build providers dialog component

---
+++ app/components/ProvidersDialog.tsx
@@ -0,0 +1,98 @@
+import { Box, Callout, Dialog, Flex, Heading, Text } from "@radix-ui/themes";
+import Image from "next/image";
+import { BsInfoCircleFill } from "react-icons/bs";
+import { CiPlay1 } from "react-icons/ci";
+import { MediaDetail } from "../hooks/useMediaDetails";
+Image;
+
+type Props = {
+  media: MediaDetail;
+};
+
+const logoPath = "https://image.tmdb.org/t/p/w200";
+
+const ProvidersDialog = ({ media }: Props) => {
+  const streamProviders = media["watch/providers"].results.US.flatrate;
+  const rentProviders = media["watch/providers"].results.US.rent;
+
+  return (
+    <Box mt="4">
+      <Dialog.Root>
+        <Dialog.Trigger>
+          <button className=" flex gap-1 items-center text-[#efc332] font-extrabold underline">
+            <CiPlay1 size="20px" />
+            Where to Watch
+          </button>
+        </Dialog.Trigger>
+        <Dialog.Content maxWidth="450px">
+          <Dialog.Title>Where to Watch</Dialog.Title>
+
+          <Heading size="2" className="uppercase">
+            Stream:
+          </Heading>
+          <Flex mt="3" direction="column" gap="2">
+            {streamProviders ? (
+              streamProviders?.length < 1 ? (
+                "Not Available"
+              ) : (
+                streamProviders?.map((provider) => (
+                  <Flex align="center" gap="3" key={provider.provider_id}>
+                    <Image
+                      src={logoPath + provider.logo_path}
+                      height={50}
+                      width={50}
+                      alt={provider.provider_name + "logo"}
+                    />
+                    <Text>{provider.provider_name}</Text>
+                  </Flex>
+                ))
+              )
+            ) : (
+              <Callout.Root>
+                <Callout.Icon>
+                  <BsInfoCircleFill />
+                </Callout.Icon>
+                <Callout.Text>
+                  Currently not available for streaming in your region
+                </Callout.Text>
+              </Callout.Root>
+            )}
+          </Flex>
+          <Heading mt="3" size="2" className="uppercase">
+            Rent:
+          </Heading>
+          <Flex mt="3" direction="column" gap="2">
+            {rentProviders ? (
+              rentProviders?.length < 1 ? (
+                "Not Available"
+              ) : (
+                rentProviders?.map((provider) => (
+                  <Flex align="center" gap="3" key={provider.provider_id}>
+                    <Image
+                      src={logoPath + provider.logo_path}
+                      height={50}
+                      width={50}
+                      alt={provider.provider_name + "logo"}
+                    />
+                    <Text>{provider.provider_name}</Text>
+                  </Flex>
+                ))
+              )
+            ) : (
+              <Callout.Root>
+                <Callout.Icon>
+                  <BsInfoCircleFill />
+                </Callout.Icon>
+                <Callout.Text>
+                  Currently not available for renting in your region
+                </Callout.Text>
+              </Callout.Root>
+            )}
+          </Flex>
+        </Dialog.Content>
+      </Dialog.Root>
+    </Box>
+  );
+};
+
+export default ProvidersDialog;

8cc15bcf6dec8a91e81d95298355739abf0796cc
Justin <alexanderrr416@gmail.com>
2024-10-16 20:13:45 -0400

    feat: add providers modal

--- app/movie/[movieId]/page.tsx
+++ app/movie/[movieId]/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
+import EmblaCarousel from "@/app/components/EmblaCarousel";
+import MovieDataList from "@/app/components/MovieDataList";
+import ProvidersDialog from "@/app/components/ProvidersDialog";
 import VideoPlayer from "@/app/components/VideoPlayer";
+import useMediaCredits from "@/app/hooks/useMediaCredits";
+import useMediaDetails from "@/app/hooks/useMediaDetails";
 import {
   Badge,
   Box,
-  Button,
   Container,
   Flex,
   Heading,
@@ -12,20 +16,15 @@ import {
   Text,
 } from "@radix-ui/themes";
 import Image from "next/image";
-import React from "react";
-import EmblaCarousel from "@/app/components/EmblaCarousel";
 import { CiHeart } from "react-icons/ci";
-import { TbPointFilled } from "react-icons/tb";
 import { FaStar } from "react-icons/fa";
-import MovieDataList from "@/app/components/MovieDataList";
-import useMediaCredits from "@/app/hooks/useMediaCredits";
-import useMediaDetails from "@/app/hooks/useMediaDetails";
+import { TbPointFilled } from "react-icons/tb";
 
 type Props = {
   params: { movieId: string };
 };
 
-const posterPath = "https://image.tmdb.org/t/p/w500";
+const mediaImgPath = "https://image.tmdb.org/t/p/w500";
 
 const MoviePage = ({ params: { movieId } }: Props) => {
   const { data: movie, isError, isLoading } = useMediaDetails(movieId, "movie");
@@ -50,17 +49,14 @@ const MoviePage = ({ params: { movieId } }: Props) => {
             width={100}
             height={100}
             alt={`${movie?.title} poster`}
-            src={`${posterPath}${movie?.poster_path}`}
+            src={`${mediaImgPath}${movie?.poster_path}`}
           />
           <Box flexGrow="1">
             <Flex gap="2" justify="between">
               <Heading as="h1" weight="light">
                 {movie?.title}
               </Heading>
-              <Button style={{ gap: "4px" }}>
-                <CiHeart color="#fff" size="20" />
-                <span className="hidden md:block">Add to Queue</span>
-              </Button>
+              <CiHeart color="#FF92B1" size="35px" />
             </Flex>
 
             <Flex className="mt-2" align="center" gap="1">
@@ -78,6 +74,7 @@ const MoviePage = ({ params: { movieId } }: Props) => {
                 </Badge>
               ))}
             </Flex>
+            {movie && <ProvidersDialog media={movie} />}
           </Box>
         </Flex>
 

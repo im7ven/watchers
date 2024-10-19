9a2a2dc185a26843749dfada3b34461b1abe0904
Justin <alexanderrr416@gmail.com>
2024-10-19 00:10:27 -0400

    feat: build modal to enlarge media posters

---
+++ app/components/ImageModal.tsx
@@ -0,0 +1,44 @@
+import { Dialog, VisuallyHidden } from "@radix-ui/themes";
+import Image, { StaticImageData } from "next/image";
+
+type Props = {
+  imgSrc: StaticImageData | string;
+  alt: string;
+};
+
+const ImageModal = ({ imgSrc, alt }: Props) => {
+  if (typeof imgSrc !== "string") {
+    return (
+      <Image
+        className="rounded md:w-[150px]"
+        width={100}
+        height={100}
+        src={imgSrc}
+        alt={alt}
+      />
+    );
+  }
+
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger>
+        <Image
+          className="rounded md:w-[150px]"
+          width={100}
+          height={100}
+          src={imgSrc}
+          alt={alt}
+        />
+      </Dialog.Trigger>
+      <VisuallyHidden>
+        <Dialog.Title>{`${alt} image`}</Dialog.Title>
+      </VisuallyHidden>
+      <Dialog.Description aria-describedby={`Enlarged ${alt} cover`} />
+      <Dialog.Content maxWidth="450px">
+        <Image width={400} height={400} src={imgSrc} alt={alt} />
+      </Dialog.Content>
+    </Dialog.Root>
+  );
+};
+
+export default ImageModal;

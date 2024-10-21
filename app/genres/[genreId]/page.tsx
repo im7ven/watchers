c39de1941adc4ef35c869d643ce7d9276fde9736
Justin <alexanderrr416@gmail.com>
2024-10-20 20:41:41 -0400

    feat: build dynamic genre page

---
+++ app/genres/[genreId]/page.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import useData from "@/app/hooks/useData";
+import { Spinner, Grid, Box } from "@radix-ui/themes";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+type MediaCover = {
+  id: number;
+  title: string;
+  poster_path: string;
+};
+
+type Props = {
+  params: { genreId: string };
+};
+
+const posterUrl = `https://image.tmdb.org/t/p/w500`;
+
+const MovieGenrePage = ({ params: { genreId } }: Props) => {
+  const { data, hasNextPage, fetchNextPage } = useData<MediaCover>(
+    "/discover/movie",
+    ["movie genre", genreId],
+    {
+      with_genres: genreId,
+    }
+  );
+
+  const fetchedMediaCount =
+    data?.pages.reduce((total, page) => total + page.results.length, 0) || 0;
+
+  return (
+    <div>
+      <InfiniteScroll
+        dataLength={fetchedMediaCount}
+        hasMore={!!hasNextPage}
+        next={() => fetchNextPage()}
+        loader={<Spinner />}
+      >
+        <Grid columns={{ initial: "3", xs: "5", sm: "6" }} gap="1" mx="auto">
+          {data?.pages.map((page, index) => (
+            <React.Fragment key={index}>
+              {page?.results.map((movie) => (
+                <Box key={movie.id}>
+                  <Link href={`movie/${movie.id}`}>
+                    <Image
+                      width={165}
+                      height={300}
+                      src={posterUrl + movie.poster_path}
+                      alt="poster"
+                    />
+                  </Link>
+                </Box>
+              ))}
+            </React.Fragment>
+          ))}
+        </Grid>
+      </InfiniteScroll>
+    </div>
+  );
+};
+
+export default MovieGenrePage;

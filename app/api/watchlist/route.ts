30061187b188276f796062d76cd2f7300a38837b
Justin <alexanderrr416@gmail.com>
2024-10-25 00:38:25 -0400

    feat: create a route handler for adding movies to watch list

---
+++ app/api/watchlist/route.ts
@@ -0,0 +1,47 @@
+import authOptions from "@/app/auth/authOptions";
+import { addMediaSchema } from "@/app/ValidationSchema";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import client from "@/lib/db";
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+
+  const validation = addMediaSchema.safeParse(body);
+
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    return NextResponse.json(
+      { error: "Must be authenticated" },
+      { status: 401 }
+    );
+  }
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  }
+
+  const { mediaId, mediaTitle, mediaType, mediaPoster } = validation.data;
+
+  try {
+    const db = (await client).db();
+
+    await db.collection("watchlist").insertOne({
+      userId: session.user.email,
+      mediaId,
+      mediaType,
+      mediaPoster,
+      mediaTitle,
+      createdAt: new Date(),
+    });
+
+    return NextResponse.json(
+      { message: `${validation.data.mediaType} added to watchlist` },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log("Error adding movie to watchlist", error);
+    return NextResponse.json({ error: "Failed to add moive" }, { status: 500 });
+  }
+}

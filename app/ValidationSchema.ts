3138a1a50311e93f01d3c7e2e730707ceb91fb8f
Justin <alexanderrr416@gmail.com>
2024-10-25 20:32:52 -0400

    chore: add props to schema

--- app/ValidationSchema.ts
+++ app/ValidationSchema.ts
@@ -3,4 +3,9 @@ import { z } from "zod";
 export const addMediaSchema = z.object({
   mediaId: z.number(),
   mediaType: z.string(),
+  mediaPoster: z.string(),
+  mediaTitle: z.string(),
+  mediaRating: z.number(),
+  mediaRuntime: z.string(),
+  mediaRelease: z.string(),
 });

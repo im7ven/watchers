92899bf776dc0cbecd3cfeda4a56c530ee86b04f
Justin <alexanderrr416@gmail.com>
2024-10-29 23:32:04 -0400

    feat: enable full screen mode

--- app/components/VideoPlayer.tsx
+++ app/components/VideoPlayer.tsx
@@ -8,6 +8,7 @@ type Props = {
 const VideoPlayer = ({ videoId, site }: Props) => {
   return (
     <iframe
+      allowFullScreen
       className="mx-auto my-8 aspect-video w-full max-w-[500px]"
       src={`https://${site}.com/embed/${videoId}`}
     >
